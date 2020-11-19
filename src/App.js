ceb5555d3f1d8a46ec87b6238c9b3f5dcf1a08eb
Rafael Enrique Vilomar Montero <rafaelvm2707@gmail.com>
2020-11-19 02:35:29 -0400

    feat: secction of cardSmall added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import './assets/styles/App.css';
+import "./assets/styles/App.css";
+import CardSmall from "./components/CardSmall";
+
+import { Container, Col, Row } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Container>
+      <h4 className="text-muted">Overview - Today</h4>
+        <Row>
+          {datas.map((data) => (
+            <Col xs={12} md={4} lg={3}>
+              <CardSmall
+                title={data.title}
+                number={data.number}
+                stats={data.stats}
+                direcction={data.direcction}
+                type={data.type}
+              />
+            </Col>
+          ))}
+        </Row>
+      </Container>
   );
 }
-
 export default App;
+
+const datas = [
+  {
+    title: "Page Views",
+    number: "87",
+    stats: "3%",
+    type: "fb",
+    direcction: "up",
+  },
+  { title: "Likes", number: "52", stats: "2%", type: "fb", direcction: "down" },
+  {
+    title: "Likes",
+    number: "5462",
+    stats: "2257%",
+    type: "ig",
+    direcction: "up",
+  },
+  {
+    title: "Profile Views",
+    number: "52k",
+    stats: "1375%",
+    type: "ig",
+    direcction: "up",
+  },
+  {
+    title: "Retweets",
+    number: "117",
+    stats: "303%",
+    type: "tw",
+    direcction: "up",
+  },
+  {
+    title: "Likes",
+    number: "507",
+    stats: "553%",
+    type: "tw",
+    direcction: "up",
+  },
+  {
+    title: "Likes",
+    number: "107",
+    stats: "19%",
+    type: "yt",
+    direcction: "down",
+  },
+  {
+    title: "Total Views",
+    number: "1407",
+    stats: "12%",
+    type: "yt",
+    direcction: "down",
+  },
+];
---
+++ src/components/CardSmall.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col, Image, Card } from "react-bootstrap";
+import {} from "../assets/styles/components/CardSmall.css";
+
+// --------------- imports images -----------------------------
+import up from "../assets/imgs/icon-up.svg";
+import down from "../assets/imgs/icon-down.svg";
+import facebook from "../assets/imgs/icon-facebook.svg";
+import instagram from "../assets/imgs/icon-instagram.svg";
+import twitter from "../assets/imgs/icon-twitter.svg";
+import youtube from "../assets/imgs/icon-youtube.svg";
+
+const CardSmall = ({ title, number, stats, type, direcction }) => {
+  return (
+    <Card.Body className="root">
+      <Container fluid>
+        <Row>
+          <Col xs={8}>
+            <p className="title"> {title} </p>
+          </Col>
+          <Col xs={4}>
+            {type === "fb" ? (
+              <Image src={facebook} />
+            ) : type === "ig" ? (
+              <Image src={instagram} />
+            ) : type === "tw" ? (
+              <Image src={twitter} />
+            ) : (
+              type === "yt" && <Image src={youtube} />
+            )}
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={7}>
+            <p className="number"> {number} </p>
+          </Col>
+          <Col xs={5} className="details">
+            {direcction === "up" ? (
+              <p className="stats" style={{ color: "hsl(163, 72%, 41%)" }}>
+                <span>
+                  <Image src={up} />
+                </span>
+                {stats}
+              </p>
+            ) : (
+              <p className="stats" style={{ color: "hsl(356, 69%, 56%)" }}>
+                <span>
+                  <Image src={down} />
+                </span>
+                {stats}
+              </p>
+            )}
+          </Col>
+        </Row>
+      </Container>
+    </Card.Body>
+  );
+};
+export default CardSmall;
